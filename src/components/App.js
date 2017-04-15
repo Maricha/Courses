59b27db1af92aa41199fdc891bf5fd942808fcee
Maricha <maricha177@gazeta.pl>
2017-04-15 16:37:59 +0200

    project init

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Header from './common/Header';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="contaier-fluid">
+                <Header/>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+App.propTypes = {
+    children: PropTypes.object.isRequired
+};
+
+export default App;
