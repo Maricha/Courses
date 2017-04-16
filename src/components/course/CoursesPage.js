f234b2ef5056876bdbdf3b66793485bbdaadd117
Maricha <maricha177@gazeta.pl>
2017-04-16 23:35:01 +0200

    manage course page

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 import * as courseActions from '../../actions/courseActions';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import {browserHistory} from 'react-router';
 
 import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props, context);
+
+        this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
+    }
+
+    redirectToAddCoursePage() {
+        browserHistory.push('/course');
     }
 
     render() {
@@ -17,6 +24,10 @@ class CoursesPage extends React.Component {
         return (
             <div>
                 <h1>Courses</h1>
+                <input type="submit"
+                    value="Add Course"
+                    className="btn btn-primary"
+                    onClick={this.redirectToAddCoursePage} />
                 <CourseList courses={courses} />
             </div>
         )
